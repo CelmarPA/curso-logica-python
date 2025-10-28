46b120c5b9fc5f64d033f59172c6ce0c25ff0e52
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2025-10-28 11:44:10 -0300

    Adicionado listas

---
+++ 9listas.py
@@ -0,0 +1,99 @@
+# Listas e Tuplas
+
+# Tipos de dados que armazenam múltiplos valores, mais têm diferenças importantes:
+
+# Listas:
+#   - Modificável (pode adicionar, remover e alterar valores)
+#   - Mais lenta
+#   - Quando precisamos modificar dados
+
+# Tuplas:
+#   - Não é modificável (uma vez criada, não pode ser alterada)
+#   - Mais rápida
+#   - Quando os dados não devem ser alterados
+
+# Lista
+# Definida entre colchetes [] e pode armazenar diferentes tipos de dados
+
+frutas = ["maçã", "banana", "laranja"]
+numeros = ["1, 2, 3, 4, 5"]
+misturada = ["Python", 3.14, True]
+
+
+# Acessando elementos da lista
+
+print(frutas[0])    # "maçã"
+print(frutas[1])    # "banana"
+print(frutas[2])    # "laranja"
+print(frutas[-1])   # "laranja" (índice negativo conta de trás para frente)
+
+
+# Alterando um valor na lista
+
+print(frutas)
+
+frutas[1] = "uva"
+print(frutas)  # ['maçã', 'uva', 'laranja']
+
+
+# Adicionando elementos à lista
+
+# append(): adiciona um item ao final
+# insert(): adiciona um item em uma posição específica
+
+numeros = [1, 2, 3]
+print(numeros)
+
+numeros.append(4)
+print(numeros)  # [1, 2, 3, 4]
+
+numeros.insert(1, 10)  # (posição, valor)
+print(numeros)  # [1, 10, 2, 3, 4] (inseriu o 10 na posição 1)
+
+
+# Removendo elementos da lista
+
+# remove(): remove um item pelo valor
+# pop(): remove um item pelo índice (ou o último item se nenhum índice for passado)
+
+frutas = ["maça", "banana", "laranja", "uva"]
+frutas.remove("banana")
+print(frutas) # "["maça", "laranja"]"
+
+frutas.pop(0)
+print(frutas) # ["laranja"]
+
+
+frutas.pop()
+print(frutas) # ["uva"]
+
+# Tuplas 
+# Tuplas são como listas, mas imutáveis. Elas são criadas com parênteses ().
+
+cores = ("vermelho", "azul", "verde")
+numeros = (1, 2, 3, 4, 5)
+
+# Acessando elementos
+print(cores[0])    # "vermelho"
+print(cores[-1])   # "verde"
+
+# Tentando modificar uma tupla (ERRO!)
+# cores[1] = "amarelo" # ❌ Isso gera um erro, pois tuplas são imutáveis!
+
+
+# Convertendo entre lista e tupla
+# Podemos converter uma tupla para uma lista e modificar os elementos.add()
+
+tupla = (1, 2, 3)
+lista = list(tupla)   # Converte para lista]
+lista.append(4)
+tupla = tuple(lista)  # Converte de volta para tupla
+print(tupla)          # (1, 2, 3, 4)
+
+
+# Quando usar tuplas?
+#   - Quando queremos garantir que os valores nao sejam alterados.
+#   - Para armazenar dados fixos como coordenadas, meses do ano, dias da semana, etc.add()
+
+meses = ("Janeiro", "Fevereiro", "Março", "Abril")
+print(meses[2])   # "Março"
