b84f8deb032c1725c51f37c8568f6594eed52964
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2025-10-28 12:19:55 -0300

    Adicionado repcond e projeto1

---
+++ 10repcond.py
@@ -0,0 +1,16 @@
+# União de Conceitos
+
+# Todas as estruturas podem ser usadas separadamente, mas
+# em um programa "real", vamos unindo todas essas estruturas
+# para criarmos os cenários que precisamos para resolver um problema!
+
+# Exemplo:
+
+# Você quer saber se um palavra contém a letra Y
+
+palavra = "Python"
+letra_procurada = "y"
+
+for letra in palavra:
+    if letra == letra_procurada:
+        print("Essa palavra tem a letra", letra_procurada)
---
+++ 11projeto1.py
@@ -0,0 +1,19 @@
+# Jogo de adivinhação
+
+# No jogo, o usuário precisa adivinhar um número secreto.add()
+# Ele pode tentar várias vezes até acertar.add()
+
+numero_secreto = 10
+
+tentativa = 0
+
+while tentativa != numero_secreto:
+    tentativa = int(input("Tente adivinha um número de 1 a 10: "))
+
+    if tentativa < numero_secreto:
+        print("O número secreto é maior!")
+    
+    elif tentativa > numero_secreto:
+        print("O número secreto é menor!")
+    else:
+        print("Parabéns, você acertou!")
