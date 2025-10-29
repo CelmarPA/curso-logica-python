c5b1fc7233552428995c5bd1ac0b1505da2c3a3d
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2025-10-29 11:40:38 -0300

    Adiciona exercicios codewars

---
+++ exercicios/exercicio2.py
@@ -0,0 +1,17 @@
+# Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+# Examples (input -> output)
+
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+def repeat_str(repeat, string):
+    if repeat > 0:
+        return string * repeat
+    else: 
+        return ""
+    
+
+print(repeat_str(-6, "I"))
+print(repeat_str(5, "Hello"))
